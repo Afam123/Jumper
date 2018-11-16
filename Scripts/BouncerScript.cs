ad2941dba9126cb7f02d341f6479973976e2ab24
Afam123 <Afamezenekwe@yahoo.com>
2018-11-15 20:26:54 -0800

    Add files via upload

---
+++ Scripts/BouncerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncerScript : MonoBehaviour
+{
+
+    public float bounceFactor = 10f;
+
+    public GameObject spawner;
+    bool hasSpawned = false;
+
+
+    void Start()
+    {
+        spawner = GameObject.FindGameObjectWithTag("Respawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            other.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceFactor, ForceMode2D.Impulse);
+        }
+            if (!hasSpawned)
+        
+{
+            spawner.SendMessage("Spawn");
+            hasSpawned = true;
+        }
+
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject player;
+	
+    
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(player.transform.position.y > 0)
+        {
+            if(player.transform.position.y > transform.position.y)
+            {
+                this.transform.position = new Vector3(5, player.transform.position.y, -10);
+            }
+        }
+
+    }
+}
